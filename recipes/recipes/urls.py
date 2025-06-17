9fc135f16f54a364152a705ecee2625d8370c84b
матвей <zopafinikovna@gmail.com>
2025-06-17 23:14:31 +0600

    add users app and a bit modifies

--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+
+from recipes import settings
+from recipesapp.views import *
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('', include("recipesapp.urls")),
     path('admin/', admin.site.urls),
+    path('account/', include('users.urls')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
